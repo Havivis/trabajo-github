343e70ba4da9bf0474963ec8a76ea9d0e96c5945
Havivis <jriveradelgado3@gmail.com>
2025-11-18 21:04:17 +0100

    Replace Main class with Calculadora for arithmetic

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
-public class Main {
-    public static void main(String[] args){
-        System.out.println("Hola mundo2");
+public class Calculadora {
+    
+    public static void main (String []args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca 2 números");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("La suma de los números es " + (a+b));
+        System.out.println("La resta de los números es " + (a-b));
+        System.out.println("La multiplicación de los números es " + (a*b));
+        if (b != 0) {
+            System.out.println("La división de los números es " + (a/b));
+        } else {
+            System.out.println("Estos números no son divisibles");
+        }
+        }
     }
-}
